a53ebd0ba6609f61d5ca708f7298d01199b6b1c4
andrekuru <andrewilliam13@gmail.com>
2023-07-31 16:32:58 -0300

    Fibonacci with memoization working

---
+++ fibonacci.py
@@ -0,0 +1,25 @@
+fibonacci_memoization = {0: 0, 1: 1}
+
+def fib(n: int) -> int:
+    if n in fibonacci_memoization:
+        return fibonacci_memoization[n]
+    
+    result = fib(n - 1) + fib(n - 2)
+    fibonacci_memoization[n] = result
+
+    return result
+
+def main():
+    print("Fibonacci: ")
+    while True:
+        try:
+            n = int(input("Entrada: "))
+            if n < 0:
+                raise(ValueError)
+
+            print(f"fib({n}) = {fib(n)}")
+
+        except ValueError:
+            print("Entrada inválida! Insira um número inteiro não negativo." )
+
+main()
