0ed060b8ab27bf90ea6a6e217a02761aee910271
andrekuru <andrewilliam13@gmail.com>
2023-08-30 20:40:16 -0300

    solution_e working

--- list2/solution_e.py
+++ list2/solution_e.py
@@ -1,44 +1,27 @@
 tests_amount = int(input())
 
-tests = list()
-_ = input()
+input()
 
-for _ in range(tests_amount):
+for test in range(tests_amount):
     words = list()
-    words_value = list()
-    line = input()
-    while line:
-        words.append(line)
-        line = input()
+    normalized = dict()
+    word = input()
+    while word:
+        words.append(word)
+        word_normalized = sorted(word.replace(" ", ""))
+        normalized[word] = word_normalized
+
+        try:
+            word = input()
+        except EOFError:
+            break
 
     words.sort()
-    tests.append(words)
-
-for t in range(len(tests)):
-    words = tests[t]
-    anagrams = dict()
-    normalized = dict()
     for i in range(len(words)):
         word = words[i]
-        if word not in normalized:
-            word_normalized = sorted(word.replace(" ", ""))
-            normalized[word] = word_normalized
-
-        for j in range(i + 1, len(words)):
-            another_word = words[j]
-            if another_word not in normalized:
-                word_normalized = sorted(another_word.replace(" ", ""))
-                normalized[another_word] = word_normalized
-            
+        for another_word in words[i + 1:]:
             if normalized[word] == normalized[another_word]:
-                if i not in anagrams:
-                    anagrams[i] = [j]
-                else:
-                    anagrams[i] += [j]
+                print(f"{word} = {another_word}")
 
-    for i in anagrams:
-        for j in anagrams[i]:
-            print(f"{words[i]} = {words[j]}")
-    
-    if t < len(tests) - 1:
-        print("")
+    if test < tests_amount - 1:
+        print()
