a35e1672b3105de10eab779446b41a0e85f2c6e5
andre.kuru <andrewilliam13@gmail.com>
2023-09-01 20:00:20 -0300

    solution b without error, but with wrong answer

--- list3/solution_b.cpp
+++ list3/solution_b.cpp
@@ -6,22 +6,22 @@
 int main()
 {
     int numbers[10000];
-    int number, i = 0;
+    int number, last = 0;
     
     while(scanf("%d", &number) != EOF){
-        numbers[i] = number;
-        int numbers_size = sizeof(numbers)/sizeof(numbers[0]);
-        int median, median_position = numbers_size / 2;
-        if (i % 2 == 0){
-            std::nth_element(numbers, numbers + median_position, numbers + numbers_size);
-            std::nth_element(numbers, numbers + median_position + 1, numbers + numbers_size);
+        numbers[last] = number;
+        // int numbers_size = sizeof(numbers)/sizeof(numbers[0]);
+        int median, median_position = last / 2;
+        std::nth_element(numbers, numbers + median_position, numbers + last + 1);
+        if (last % 2 == 1){
+            std::nth_element(numbers, numbers + median_position + 1, numbers + last + 1);
             auto middle_1 = numbers[median_position];
             auto middle_2 = numbers[median_position + 1];
-            median = middle_1 / middle_2;
+            median = (middle_1 + middle_2) / 2;
         } else{
-            std::nth_element(numbers, numbers + median_position, numbers + numbers_size);
             median = numbers[median_position];
         }
         printf("%d\n", median);
+        last++;
     }
 }
