8f62fe8da42a4cab72373871262cb7dd9b432ad6
andrekuru <andrewilliam13@gmail.com>
2023-09-16 20:33:22 -0300

    solution b fixed

--- list3/solution_b.cpp
+++ list3/solution_b.cpp
@@ -8,18 +8,17 @@ int main()
     int numbers[10000];
     int number, last = 0;
     
-    while(scanf("%d", &number) != EOF){
+    while(scanf("%d ", &number) != EOF){
         numbers[last] = number;
-        // int numbers_size = sizeof(numbers)/sizeof(numbers[0]);
         int median, median_position = last / 2;
         std::nth_element(numbers, numbers + median_position, numbers + last + 1);
+        auto middle_1 = numbers[median_position];
         if (last % 2 == 1){
             std::nth_element(numbers, numbers + median_position + 1, numbers + last + 1);
-            auto middle_1 = numbers[median_position];
             auto middle_2 = numbers[median_position + 1];
             median = (middle_1 + middle_2) / 2;
         } else{
-            median = numbers[median_position];
+            median = middle_1;
         }
         printf("%d\n", median);
         last++;
