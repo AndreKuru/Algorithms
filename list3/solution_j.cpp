d32052abaf95733b25b8705f7258e73c36ec3c92
andrekuru <andrewilliam13@gmail.com>
2023-09-17 00:32:04 -0300

    solution j working

--- list3/solution_j.cpp
+++ list3/solution_j.cpp
@@ -4,25 +4,37 @@
 
 int main()
 {
-    int tests_amount, friends_amount, stamps_amount;
-    std::map<int, std::set<int>> stamps_owneds;
+    int tests_amount, friends_amount, stamps_amount, stamp_id;
 
     scanf("%d", &tests_amount);
     for (int test = 0; test < tests_amount; test++) {
+        std::map<int, std::set<int>> stamps_owneds;
+        std::map<int, double> unique_owners;
 
         scanf("%d", &friends_amount);
         for (int friend_id = 0; friend_id < friends_amount; friend_id++) {
 
             scanf("%d", &stamps_amount);
             for (int stamp = 0; stamp < stamps_amount; stamp++) {
-                if (stamps_owneds.find(stamp) == stamps_owneds.end()) {
-                    stamps_owneds[stamp].insert(friend_id);
-                } else {
-                    std::set<int> friends_owners;
-                    friends_owners.insert(friend_id);
-                    stamps_owneds[stamp] = friends_owners;
-                }
+                scanf("%d", &stamp_id);
+                stamps_owneds[stamp_id].insert(friend_id);
             }
         }
+        auto total_of_uniques = 0;
+        for (auto it = stamps_owneds.begin(); it != stamps_owneds.end(); it++) {
+            if (it->second.size() == 1) {
+                auto friend_id = *it->second.begin();
+                unique_owners[friend_id]++;
+                auto uniques = unique_owners[friend_id];
+                total_of_uniques++;
+            }
+        }
+
+        printf("Case %d:", test + 1);
+        for (int friend_id = 0; friend_id < friends_amount; friend_id++) {
+            auto percent = unique_owners[friend_id] / total_of_uniques * 100;
+            printf(" %f%%", percent);
+        }
+        printf("\n");
     }
 }
