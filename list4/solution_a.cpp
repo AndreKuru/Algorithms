757072de488fdd75dcf345240ae1a9976805a9d8
andrekuru <andrewilliam13@gmail.com>
2023-09-19 12:41:50 -0300

    implemented my own next_permutation for solution a

--- list4/solution_a.cpp
+++ list4/solution_a.cpp
@@ -41,12 +41,43 @@ void try_num(std::string& num_str, int expected_result, std::vector<std::pair<st
         answers.push_back(answer);
     }
 }
+
+bool next_numerator(std::string& number_str){
+    auto found = false;
+    auto it = std::prev(number_str.end());
+    for (; it != number_str.begin(); --it) {
+        if (*std::prev(it) < *it) {
+            found = true;
+            break;
+        }
+    }
+
+    if (not found) {
+        return false;
+    }
+
+
+    auto a = std::prev(it);
+    auto b = it;
+    ++it;
+    for (; it != number_str.end(); ++it) {
+        if (*it <= *b and *it > *a) {
+            b = it;
+        }
+    }
+
+    std::swap(*a, *b);
+    std::reverse(std::next(a), number_str.end());
+    return true;
+}
+
 int main()
 {
     int expected_result;
     auto blank_line = false;
     scanf("%d%*c", &expected_result);
     while(expected_result != 0) {
+        auto cont = 0;
         if (blank_line) {
             printf("\n");
         } else {
@@ -60,9 +91,13 @@ int main()
             try_num(num_str, expected_result, answers);
             auto old_numerator = num_str.substr(0, 5);
             do {
-                is_new_num_str = std::next_permutation(num_str.begin(), num_str.end());
-            } while(old_numerator == num_str.substr(0, 5)); 
+                ++cont;
+                // printf("%s\n", num_str.c_str());
+                is_new_num_str = next_numerator(num_str);
+                // is_new_num_str = std::next_permutation(num_str.begin(), num_str.end());
+            } while(old_numerator == num_str.substr(0, 5) and is_new_num_str); 
         }
+        printf("cont: %d\n", cont);
 
         if (answers.empty()) {
             printf("There are no solutions for %d.\n", expected_result);
