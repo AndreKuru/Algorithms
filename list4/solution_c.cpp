bd3b8dad8b8567c7c91757f8d1905ac7dd0fe71d
AndreKuru <andrewilliam13@gmail.com>
2023-09-20 16:44:24 -0300

    solution c implemented, but problem with shift

---
+++ list4/solution_c.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    int tests_amount;
+    scanf("%d%*c", &tests_amount);
+
+    for (auto test = 0; test < tests_amount; ++test) {
+        int bars_length, bars_amount, bar;
+        auto bars = std::vector<int> {};
+        scanf("%d%*c", &bars_length);
+        scanf("%d%*c", &bars_amount);
+        for (auto i = 0; i < bars_amount; ++i) {
+            scanf("%d%*c", &bar);
+            bars.push_back(bar);
+        }
+
+        auto has_solution = false;
+        auto bmask_limit = 1 << bars_amount;
+        for (auto bmask = 0; bmask < bmask_limit; ++bmask) {
+            auto selected_bars_sum = 0;
+            for (auto i = 0; i <= bmask; ++i) {
+                printf("limit=%d bmask=%d i=%d b=%d\n", bmask_limit, bmask, i, 1 << i -1);
+                selected_bars_sum += bmask & (1 << i) - 1;
+            }
+
+            if (selected_bars_sum == bars_length) {
+                has_solution = true;
+                break;
+            }
+        }
+
+        if (has_solution) {
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
+    }
+}
